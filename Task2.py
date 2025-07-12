ffa454cb6d11091e38f923484495b766dd283f84
Debabrata Mishra <121426115+Devofficial31@users.noreply.github.com>
2025-07-12 11:36:11 +0530

    Add files via upload

---
+++ Task2.py
@@ -0,0 +1,6 @@
+from xml.dom.minidom import ProcessingInstruction
+
+a=input("Enter your First Name: ")
+b=input("Enter Your Second Name: ")
+fullName=a + " " + b
+print(f"hello ,{fullName}! Welcome to python program.")
